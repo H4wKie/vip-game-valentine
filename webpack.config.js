0436e94490aabd2d098d64bdd1559f4939e58fd2
H4wKie <lousrus@outlook.com>
2025-01-19 16:45:51 +0100

    updated blue highlight again

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const historyApiFallback = require('connect-history-api-fallback');
 
 module.exports = {
-  mode: 'production',
+  mode: 'development',
   entry: {
     bundle: path.resolve(__dirname, 'src/index.js')
   },
